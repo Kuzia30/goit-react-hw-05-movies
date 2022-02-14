2314db804e0bec750dc1770811d665d7412e0473
Kuzia30 <maxzimka37@gmail.com>
2022-02-14 17:47:19 +0200

    fixed suspense

--- src/App.js
+++ src/App.js
@@ -4,27 +4,71 @@ import { GlobalStyle } from "./App.styled";
 import Navigation from "./components/Navigation";
 import Loader from "./components/Loader";
 
-const HomeView = lazy(() => import("./vievs/Home.js"));
-const MoviesView = lazy(() => import("./vievs/Movies.js"));
-const FilmView = lazy(() => import("./vievs/Film.js"));
-const Cast = lazy(() => import("./vievs/Cast.js"));
-const Reviews = lazy(() => import("./vievs/Reviews.js"));
+const HomeView = lazy(() =>
+  import("./vievs/Home.js" /* webpackChunkName: "HomeView" */)
+);
+const MoviesView = lazy(() =>
+  import("./vievs/Movies.js" /* webpackChunkName: "MoviesView" */)
+);
+const FilmView = lazy(() =>
+  import("./vievs/Film.js" /* webpackChunkName: "FilmView" */)
+);
+const Cast = lazy(() =>
+  import("./vievs/Cast.js" /* webpackChunkName: "Cast" */)
+);
+const Reviews = lazy(() =>
+  import("./vievs/Reviews.js" /* webpackChunkName: "Reviews" */)
+);
 
 const App = () => (
   <>
     <GlobalStyle />
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route index element={<HomeView />} />
-          <Route path="movies" element={<MoviesView />} />
-          <Route path="movies/:itemId" element={<FilmView />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="revievs" element={<Reviews />} />
-          </Route>
+
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loader />}>
+              <HomeView />
+            </Suspense>
+          }
+        />
+        <Route
+          path="movies"
+          element={
+            <Suspense fallback={<Loader />}>
+              <MoviesView />
+            </Suspense>
+          }
+        />
+        <Route
+          path="movies/:itemId"
+          element={
+            <Suspense fallback={<Loader />}>
+              <FilmView />
+            </Suspense>
+          }
+        >
+          <Route
+            path="cast"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Cast />
+              </Suspense>
+            }
+          />
+          <Route
+            path="revievs"
+            element={
+              <Suspense fallback={<Loader />}>
+                <Reviews />
+              </Suspense>
+            }
+          />
         </Route>
-      </Routes>
-    </Suspense>
+      </Route>
+    </Routes>
   </>
 );
 export default App;
